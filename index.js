1802a4e11a2aee71f00ccd36f0bb1d0e50605cfc
Renée Cruz <reneebasscruz@gmail.com>
2019-02-20 20:54:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(katzDeliLine) {
   // code block to be executed
   return (`The line is currently: ${i+1}. ${katzDeliLine[i]}`);
 }
-    return ("The line is currently empty.");
+  return (`The line is currently: ${i+1}.
 }
   
 
