0b5132f147c9cbbe61461e973b29b2196d1fa7e7
Renée Cruz <reneebasscruz@gmail.com>
2019-02-20 20:43:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine = katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine} in line.`);
+}
+
+//for above Welcome Message, consider using String Interpolation Template Literals? 
+
+function nowServing(katzDeliLine) {
+ 
+  if
+    (katzDeliLine.length >= 1) {
+      
+      return "Currently serving " + katzDeliLine.shift() + ".";
+    }
+  else {
+      return ("There is nobody waiting to be served!");
+  }
+}
+
+function currentLine(katzDeliLine) {
+  while (katzDeliLine > 0) {
+  // code block to be executed
+  return (`The line is currently: ${katzDeliLine}`);
+}
+    return ("The line is currently empty.");
+}
+  
+
